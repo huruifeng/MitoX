71b93f04e7e046dfac7aefd8b75a0a6dceefd310
RuifengHu <442490020@qq.com>
2020-11-07 11:37:40 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setuptools.setup(
 	description="exploring mitochondrial variants and gene expressions from single cell sequencing assays",
 	author='Ruifeng Hu',
 	author_email="huruifeng.cn@hotmail.com",
-	packages=['mitox'],
+	packages=['mitox','mitox.data'],
 	install_requires=['sklearn','seaborn','matplotlib','pysam','HTseq','anndata','pandas','numpy', 'scipy','umap-learn']
 )
