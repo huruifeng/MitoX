8b9890c4c93ebe423abddde7f2080e625ad0a68a
RuifengHu <442490020@qq.com>
2020-11-07 19:06:32 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,7 @@ setuptools.setup(
 	author='Ruifeng Hu',
 	author_email="huruifeng.cn@hotmail.com",
 	packages=['mitox','mitox.data'],
+	package_data={'mitox': ['data/human_mt.fasta','data/human_mt.fasta.fai','data/MusMT.fasta','data/MusMT.fasta.fai']},
+	include_package_data=True,
 	install_requires=['sklearn','seaborn','matplotlib','pysam','HTseq','anndata','pandas','numpy', 'scipy','umap-learn']
 )
