e9c0da1dd6a4cbe8727f6fbb73ab4d7df4756779
RuifengHu <442490020@qq.com>
2020-11-07 19:20:31 -0600

    Update

--- mitox/__init__.py
+++ mitox/__init__.py
@@ -1,9 +1,8 @@
 from .version import VERSION as __version__
-print("Version %s" % __version__)
 
 from .bamreader import *
 from .utils import *
 from .expression import *
 from .funcs import *
 
-
+print("MitoX version %s" % __version__)
